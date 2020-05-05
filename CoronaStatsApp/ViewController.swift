b2d187007d70043eb9a277cc8cc5fffd8e5338fd
yuji-nakamoto <yuji0411@icloud.com>
2020-05-05 09:25:58 +0900

    Initial Commit

---
+++ CoronaStatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaStatsApp
+//
+//  Created by yuji_nakamoto on 2020/05/05.
+//  Copyright © 2020 yuji_nakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
